1d1f2225b38b45040ced1416080ccb21c35a4dc2
Dave Powers <djpowers89@gmail.com>
2013-12-23 00:15:52 -0500

    Implement dog model and tests.

--- db/migrate/20131223051004_create_dogs.rb
+++ db/migrate/20131223051004_create_dogs.rb
@@ -1,6 +1,7 @@
 class CreateDogs < ActiveRecord::Migration
   def change
     create_table :dogs do |t|
+      t.string :name
 
       t.timestamps
     end
--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Dog do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should have_valid(:name).when("Sparky", "Sophie", "Big Rex") }
+  it { should_not have_valid(:name).when(nil, "") }
+
 end
